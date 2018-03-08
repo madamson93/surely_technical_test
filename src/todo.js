39766e1607f253b8df58a9cb2ca8e5bbf4dbde66
Monique Adamson <madamson@hotmail.co.uk>
2018-03-08 12:48:42 +0000

    Create front end for to-do list application
    Add more functions to Angular controller and bind with the front end markup

--- src/todo.js
+++ src/todo.js
@@ -12,19 +12,59 @@ app.controller('TodoListController', function($scope, $http) {
             });
     };
 
-    //load data on page load
     $scope.loadData();
-
 
     //POST request to create a new task in the backend
     $scope.submitTaskForm = function(){
-        var postData = {
-            task_details: $scope.taskdetails
+        if($scope.taskID != undefined) {
+            var editData = {
+                task_id: $scope.taskID,
+                task_details: $scope.taskdetails
+            };
+
+            $http.post('../api/update.php', editData)
+                .then(function() {
+                    $scope.taskdetails = null;
+                });
+
+        } else {
+            var postData = {
+                task_details: $scope.taskdetails
+            };
+
+            $http.post('../api/create.php', postData)
+                .then(function() {
+                    $scope.taskdetails = null;
+                })
+        }
+
+        $scope.loadData();
+    };
+
+    //DELETE request to delete a task
+    $scope.deleteTask = function(taskID){
+        var deleteData = {
+          task_id: taskID
         };
 
-        $http.post('../api/create.php', postData)
+        $http.post('../api/delete.php', deleteData)
             .then(function() {
                 $scope.loadData();
         })
     };
+
+    //GET request to load existing task into form
+    $scope.editTask = function(taskID) {
+        $scope.taskFound = [];
+        $scope.taskID = taskID;
+
+        //retrieve the task using the API
+        $http.get("../api/readbyid.php", {
+            params: {
+                task_id: taskID
+            }
+        }).then(function(response) {
+            $scope.taskdetails = response.data[0].task_details;
+        });
+    }
 });
