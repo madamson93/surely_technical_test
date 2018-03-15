8d7dee68479de46fb2155f255aff6712caae726d
Monique Adamson <madamson@hotmail.co.uk>
2018-03-15 22:02:41 +0000

    Create Task entity class
    Represent the tasks in the database as an entity class

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moniqueadamson
+ * Date: 15/03/2018
+ * Time: 21:50
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="surely_tasks")
+ */
+class Task
+{
+	/**
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=256)
+	 */
+	private $task_details;
+
+	/**
+	 * @return mixed
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getTaskDetails()
+	{
+		return $this->task_details;
+	}
+
+	/**
+	 * @param mixed $task_details
+	 */
+	public function setTaskDetails($task_details)
+	{
+		$this->task_details = $task_details;
+	}
+
+
+}
